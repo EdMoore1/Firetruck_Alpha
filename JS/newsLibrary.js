5cad988b0e5de7b00608e07e399bc2582e7fa138
Ed Moore <edward.moore@students.mq.edu.au>
2013-05-13 06:59:29 +1000

    News Feed Library
    Included a news feed library that will scroll across random news
    articles

---
+++ JS/newsLibrary.js
@@ -0,0 +1,23 @@
+function NewsFeed() {
+    //CONFIG Settings
+    "use strict";
+    NewsFeed.canvasWidth = 1024;
+    NewsFeed.canvasHeight = 640;
+    NewsFeed.moveSpeed = 5;
+    var c = document.getElementById("news");
+    var canvas = c.getContext("2d");
+
+
+    var items = ["news 1", "news 2", "news 3"];
+
+
+
+
+
+    NewsFeed.prototype.start = function() {
+
+    }
+
+    //TIMER
+    
+}
